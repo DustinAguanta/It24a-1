0b8f37ba15e754a7ac599975ac288066a45e396b
jraldGITHUB <jraldybaco@gmail.com>
2024-10-07 14:17:17 +0300

    modified navbar and change marker location

--- applet-2.js
+++ applet-2.js
@@ -34,8 +34,8 @@ const Mymap = new leafLetMap('map', [8.359735, 124.869206], 18);
 
 
 Mymap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
-Mymap.addMarker(8.359639, 124.869179, 'CCS Laboratory 1');
-Mymap.addMarker(8.359554, 124.869153, 'CCS Laboratory 2');
-Mymap.addMarker(8.359554, 124.869153, 'CCS Laboratory 2');
+Mymap.addMarker(8.359554, 124.869153, 'Sc Building');
+Mymap.addMarker(8.359554, 124.869153, 'CCS Laborotories 2');
+
 
 Mymap.loadMarkersFromJson('map.json');
